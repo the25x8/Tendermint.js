af3cfd83e24d279489dbbb14358421526db48855
cryptoji <kocherginji@gmail.com>
2019-01-11 11:59:55 +0300

    ~ update index

--- src/index.ts
+++ src/index.ts
@@ -2,138 +2,9 @@ const { decode } = require('msgpack-lite');
 const Observable = require('zen-observable');
 const WS = require('ws');
 
-/*
- * Block interfaces
- */
-export interface IBlockID {
-	hash: string;
-	parts: {
-		hash: string;
-		total: string;
-	};
-}
-
-export interface IBlockPrecommit {
-	type: string;
-	round: string;
-	height: string;
-	validator_index: string;
-	validator_address: string;
-	signature: {
-		data: string;
-		type: string;
-	};
-	block_id: IBlockID;
-}
-
-export interface IBlockLastCommit {
-	precommits: IBlockPrecommit[];
-	blockID: IBlockID;
-}
-
-export interface IBlockData {
-	txs: string[];
-}
-
-export interface IBlockHeader {
-	app_hash: string;
-	chain_id: string;
-	height: string;
-	time: string;
-	num_txs: string;
-	last_block_id: IBlockID;
-	last_commit_hash: string;
-	data_hash: string;
-	validators_hash: string;
-}
-
-export interface IBlockMeta {
-	header: IBlockHeader;
-	block_id: IBlockID;
-}
-
-// https://github.com/tendermint/tendermint/blob/master/rpc/core/blocks.go#L145
-export interface IBlock {
-	last_commit: IBlockLastCommit;
-	data: IBlockData;
-	header: IBlockHeader;
-	block_meta?: IBlockMeta;
-}
-
-/*
- * -------------------
- * Transaction interfaces
- */
-export interface ITxProof {
-	Data: string;
-	RootHash: string;
-	Total: string;
-	Index: string;
-	Proof: {
-		aunts: string[],
-	};
-}
-
-export interface ITxResult {
-	log?: string;
-	data?: string;
-	code?: string;
-}
-
-// https://github.com/tendermint/tendermint/blob/master/rpc/core/tx.go#L37
-export interface ITx {
-	proof: ITxProof;
-	index: string;
-	height: string;
-	hash: string;
-	tx: string;
-	tx_result: ITxResult;
-}
-
-/*
- * Status interfaces
- */
-export interface IStatusValidatorInfo {
-	voting_power: string;
-	address: string;
-	pub_key: {
-		type: string;
-		value: string;
-	};
-}
-
-export interface IStatusSyncInfo {
-	latest_block_hash: string;
-	latest_app_hash: string;
-	latest_block_height: string;
-	latest_block_time: string;
-	catching_up: boolean;
-}
-
-// https://github.com/tendermint/tendermint/blob/master/rpc/core/status.go#L38
-export interface IStatusNodeInfo {
-	id: string;
-	listen_addr: string;
-	network: string;
-	version: string;
-	channels: string;
-	moniker: string;
-	other: {
-		tx_index: string;
-		rpc_addr: string;
-	};
-	protocol_version: {
-		p2p: string;
-		block: string;
-		app: string;
-	};
-}
-
-export interface IStatus {
-	node_info: IStatusNodeInfo;
-	sync_info: IStatusSyncInfo;
-	validator_info: IStatusValidatorInfo;
-}
+import { TxModel } from './models/tx';
+import { BlockModel } from './models/block';
+import { IStatus } from './interfaces/status';
 
 export interface IClientOptions {
 	node_rpc: string;
@@ -141,44 +12,18 @@ export interface IClientOptions {
 	autoSyncToWs?: boolean;
 }
 
-/*
- * ----------------------
- * Common models
- */
-export class BlockModel implements IBlock {
-	last_commit: IBlockLastCommit;
-	data: IBlockData;
-	header: IBlockHeader;
-	block_meta: IBlockMeta;
+export class TendermintClientWS {
+	private connection: any; // Ws connection
+	private options: IClientOptions;
+	readonly isNode = false;
 
-	constructor(rawBlock: IBlock) {
-		this.last_commit = rawBlock.last_commit;
-		this.block_meta = rawBlock.block_meta;
-		this.header = rawBlock.header;
-		this.data = rawBlock.data;
-	}
-}
-
-export class TxModel {
-	constructor(rawTx: ITx) {}
-}
-
-/*
- * Main class
- */
-export class TendermintClient {
-	public status: IStatus; // Last sync node status
 	public events: any; // Observer events
 	public isSynced: boolean;
 
-	readonly isNode: boolean; // Select platform
-	private connection: any; // Ws connection
-	private options: IClientOptions;
-
-	constructor(options: IClientOptions) {
-		this.options = options;
+	constructor(globalProps: object) {
 		this.isSynced = false;
 		this.isNode = typeof window === 'undefined';
+		this.options = globalProps.options;
 	}
 
 	/*
@@ -277,6 +122,20 @@ export class TendermintClient {
 		} catch (e) {
 			console.error(e);
 		}
+	}
+}
+
+/*
+ * Main class
+ */
+export class TendermintClient extends TendermintClientWS<object> {
+	private options: IClientOptions;
+
+	public status: IStatus; // Last sync node status
+
+	constructor(options: IClientOptions) {
+		super({ options });
+		this.options = options;
 	}
 
 	public test() {
