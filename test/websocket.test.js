afef3361818db32d3e28d7680626b67f60319323
cryptoji <kocherginji@gmail.com>
2019-01-18 00:18:12 +0300

    ~ Check subscriptions in websocket test; fix testing logic;

--- test/websocket.test.js
+++ test/websocket.test.js
@@ -1,5 +1,6 @@
 const { TendermintClient } = require('../lib/index');
 
+const blocks = [];
 const instance = new TendermintClient({
 	node_rpc: 'http://localhost:26657',
 	node_ws: 'ws://localhost:26657'
@@ -9,6 +10,11 @@ instance.connect({
 	subscribeTo: ['blocks']
 });
 
-instance.$eventsSubscription.subscribe(data => {
-	console.log(data);
+if (!instance.$socketEventsSubscription) {
+	console.error('Web socket connection error');
+}
+
+instance.$socketEventsSubscription.subscribe(data => {
+	blocks.push(data);
+	console.log(blocks);
 });
