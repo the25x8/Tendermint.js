454a53d94764324b0988e69174b7ca60fd3939c4
cryptoji <kocherginji@gmail.com>
2019-01-24 15:14:54 +0300

    ~ united webpack.config for node and web build

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+// Build mode
+const mode = process.env.MODE ? process.env.MODE : 'development';
+const target = process.env.TARGET ? process.env.TARGET : 'node';
+
+module.exports = {
+	output: {
+		path: path.resolve(__dirname, 'lib'),
+		filename: 'tendermint.js',
+		libraryTarget: 'umd',
+		library: 'tendermintJs',
+		umdNamedDefine: true
+	},
+	target,
+	mode,
+	entry: './src/index.ts',
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.js$/,
+				use: ['source-map-loader'],
+				enforce: 'pre'
+			}
+		]
+	}
+}
