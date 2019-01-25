78ea8c1c425f532985e30171fcdfc8968e169015
cryptoji <kocherginji@gmail.com>
2019-01-25 17:47:05 +0300

    ~ add libraryExport to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
 const path = require('path');
 
-// Build mode
-const mode = process.env.MODE ? process.env.MODE : 'development';
-const target = process.env.TARGET ? process.env.TARGET : 'node';
+module.exports = env => {
+	// Build mode
+	const mode = env.MODE ? env.MODE : 'development';
+	const target = env.TARGET ? env.TARGET : 'node';
+	const outputFilename = target === 'web' ? 'tendermint.umd.js' : 'tendermint.js';
 
-module.exports = {
-	output: {
-		path: path.resolve(__dirname, 'lib'),
-		filename: 'tendermint' + target === 'web' ? '.umd.js' : '.js',
-		libraryTarget: 'umd',
-		library: 'tendermintJs',
-		umdNamedDefine: true
-	},
-	target,
-	mode,
-	entry: './src/index.ts',
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx|js|jsx)$/,
-				loader: 'babel-loader',
-			},
-			{
-				test: /\.js$/,
-				use: ['source-map-loader'],
-				enforce: 'pre'
-			}
-		]
-	}
+	return {
+		output: {
+			path: path.resolve(__dirname, 'lib'),
+			filename: outputFilename,
+			libraryTarget: 'umd',
+			library: 'TendermintJS',
+			libraryExport: 'default'
+		},
+		target,
+		mode,
+		entry: './src/index.ts',
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx|js|jsx)$/,
+					loader: 'babel-loader',
+				},
+				{
+					test: /\.js$/,
+					use: ['source-map-loader'],
+					enforce: 'pre'
+				}
+			]
+		}
+	};
 }
