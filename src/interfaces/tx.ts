ad482e4a1320f1227b55fd4012d9db70a7f801c1
cryptoji <kocherginji@gmail.com>
2019-01-27 00:24:30 +0300

    ~ update Tx interface

--- src/interfaces/tx.ts
+++ src/interfaces/tx.ts
@@ -12,18 +12,27 @@ export interface ITxProof {
   };
 }
 
+export interface ITxResultTag {
+	key: string;
+	value: string;
+}
+
 export interface ITxResult {
   log?: string;
   data?: string;
   code?: string;
+  tags?: ITxResultTag[];
 }
 
 // https://github.com/tendermint/tendermint/blob/master/rpc/core/tx.go#L37
 export interface ITx {
   proof: ITxProof;
-  index: string;
-  height: string;
+  index: number;
+  height: number;
   hash: string;
   tx: string;
   tx_result: ITxResult;
+  result?: {
+    tags: ITxResultTag[];
+  }
 }
