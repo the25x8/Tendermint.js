2d1dc42386e04c5af6b2def510250209f39ac978
cryptoji <kocherginji@gmail.com>
2019-01-27 00:24:12 +0300

    ~ decoded txs are in BlockModel

--- src/models/block.ts
+++ src/models/block.ts
@@ -1,3 +1,6 @@
+const { decode } = require('msgpack-lite');
+const Buffer = require('buffer/').Buffer;
+
 import {
   IBlock,
   IBlockData,
@@ -12,10 +15,19 @@ export class BlockModel implements IBlock {
   public header: IBlockHeader;
   public block_meta: IBlockMeta;
 
+  // Additional props
+  public txs: any[] = [];
+
   constructor(rawBlock: IBlock) {
     this.last_commit = rawBlock.last_commit;
     this.block_meta = rawBlock.block_meta;
     this.header = rawBlock.header;
     this.data = rawBlock.data;
+
+    if (rawBlock.data.txs) {
+	    this.txs = rawBlock.data.txs.map(
+	      txBytes => decode(Buffer.from(txBytes, 'base64'))
+      );
+    }
   }
 }
