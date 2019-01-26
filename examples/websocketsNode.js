7a2b14bd290fd0987fe01cd6459b29cef8582e86
cryptoji <kocherginji@gmail.com>
2019-01-26 15:02:25 +0300

    ~ tabs to spaces fix2

--- examples/websocketsNode.js
+++ examples/websocketsNode.js
@@ -1,23 +1,23 @@
 const TendermintJS = require('../lib/tendermint.js');
 
 const options = {
-	node_rpc: 'http://localhost:26657',
-	node_ws: 'ws://localhost:26657',
-	logs: true
+  node_rpc: 'http://localhost:26657',
+  node_ws: 'ws://localhost:26657',
+  logs: true
 };
 
 const tmJs = new TendermintJS(options);
 
 // Connect to ws
 tmJs.socket.connect(['blocks'])
-	.then(() => {
-		console.info('Tendermint.connect: Connected to node websocket');
-	})
-	.catch(() => {
-		console.error('Tendermint.connect: Websocket connection error');
-	});
+  .then(() => {
+    console.info('Tendermint.connect: Connected to node websocket');
+  })
+  .catch(() => {
+    console.error('Tendermint.connect: Websocket connection error');
+  });
 
 // Subscribe to events
 tmJs.socket.$events.subscribe(data => {
-	console.log(data);
+  console.log(data);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
 const path = require('path');
 
 module.exports = env => {
-	// Build mode
-	const mode = env.MODE ? env.MODE : 'development';
-	const target = env.TARGET ? env.TARGET : 'node';
-	const outputFilename = target === 'web' ? 'tendermint.umd.js' : 'tendermint.js';
+  // Build mode
+  const mode = env.MODE ? env.MODE : 'development';
+  const target = env.TARGET ? env.TARGET : 'node';
+  const outputFilename = target === 'web' ? 'tendermint.umd.js' : 'tendermint.js';
 
-	return {
-		output: {
-			path: path.resolve(__dirname, 'lib'),
-			filename: outputFilename,
-			libraryTarget: 'umd',
-			library: 'TendermintJS',
-			libraryExport: 'default'
-		},
-		target,
-		mode,
-		entry: './src/index.ts',
-		resolve: {
-			extensions: ['.ts', '.js'],
-		},
-		module: {
-			rules: [
-				{
-					test: /\.(ts|js)$/,
-					loader: 'babel-loader',
-				},
-				{
-					test: /\.js$/,
-					use: ['source-map-loader'],
-					enforce: 'pre'
-				}
-			]
-		}
-	};
+  return {
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: outputFilename,
+      libraryTarget: 'umd',
+      library: 'TendermintJS',
+      libraryExport: 'default'
+    },
+    target,
+    mode,
+    entry: './src/index.ts',
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)$/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.js$/,
+          use: ['source-map-loader'],
+          enforce: 'pre'
+        }
+      ]
+    }
+  };
 }
