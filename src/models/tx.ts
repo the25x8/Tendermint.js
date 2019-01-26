a3fa4447c44272166ffcc95867b3ef4e1212d4ca
cryptoji <kocherginji@gmail.com>
2019-01-27 00:23:17 +0300

    ~ complete the TxModel class

--- src/models/tx.ts
+++ src/models/tx.ts
@@ -1,5 +1,36 @@
-import { ITx } from '../interfaces/tx';
+const { decode } = require('msgpack-lite');
+const Buffer = require('buffer/').Buffer;
 
-export class TxModel {
-  constructor(rawTx: ITx) {}
+import { ITx, ITxProof, ITxResult } from '../interfaces/tx';
+
+export class TxModel implements ITx {
+  public proof: ITxProof;
+  public tx: string;
+	public tx: string;
+	public tx_result: ITxResult;
+  public index: number;
+  public height: number;
+  public hash: string;
+
+  // Additional props
+  public tx_decoded: string;
+	public result: { tags: object } = { tags: null };
+
+  constructor(rawTx: ITx) {
+	  this.proof = rawTx.proof;
+    this.tx = rawTx.tx;
+    this.tx_decoded = decode(Buffer.from(rawTx.tx, 'base64'));
+    this.tx_result = rawTx.tx_result || rawTx.result;
+    this.index = rawTx.index;
+    this.height = rawTx.height;
+    this.hash = rawTx.hash;
+
+    if (this.tx_result.tags) {
+	    this.result.tags = this.tx_result.tags.reduce(
+		    (memo, group) => {
+			    memo[atob(group.key)] = group.value ? atob(group.value) : null;
+			    return memo;
+		    }, {});
+    }
+  }
 }
