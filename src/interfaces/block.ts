614818e0281a1a591ce99fd3fa9076c2ae605a5f
cryptoji <kocherginji@gmail.com>
2019-01-26 14:55:46 +0300

    ~ add new properties to the interface of BlockHeader

--- src/interfaces/block.ts
+++ src/interfaces/block.ts
@@ -41,6 +41,8 @@ export interface IBlockHeader {
   last_commit_hash: string;
   data_hash: string;
   validators_hash: string;
+  consensus_hash: string;
+  proposer_address: string;
 }
 
 export interface IBlockMeta {
