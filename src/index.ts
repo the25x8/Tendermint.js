97dcdad39cb3e2389595965eb7b7575cce3eab5f
cryptoji <kocherginji@gmail.com>
2019-01-26 14:57:02 +0300

    + static factory methods createBlockModel and createTxModel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import '@babel/polyfill';
 import { IStatus } from './interfaces/status';
+
+// Base classes
 import { RpcClient } from './rpcClient';
 import { SocketClient } from './socketClient';
+
+// Import models and interfaces
+import { BlockModel } from './models/block';
+import { IBlock } from './interfaces/block';
+import { TxModel } from './models/tx';
+import { ITx } from './interfaces/tx';
 
 export interface IClientOptions {
   node_rpc: string;
@@ -12,9 +20,9 @@ export interface IClientOptions {
 /*
  * This is abstract wrapper
  * for RpcClient and SocketClient
+ * provides factories of models
  */
 export default class TendermintJs {
-
   public status: IStatus; // Last sync node status
   public socket: SocketClient;
   public rpc: RpcClient;
@@ -27,6 +35,14 @@ export default class TendermintJs {
     this.socket = new SocketClient(options);
     this.rpc = new RpcClient(options);
   }
+
+	public static createBlockModel(type, rawBlock: IBlock): BlockModel {
+		return new BlockModel(rawBlock);
+	}
+
+	public static createTxModel(rawTx: ITx): TxModel {
+    return new TxModel(rawTx);
+	}
 
   public test() {
     return 'ok';
