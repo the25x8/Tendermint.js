1fcb11140549622d17d879d5076784a9691a8e48
cryptoji <kocherginji@gmail.com>
2019-01-27 15:01:14 +0300

    ~ code/style fixes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import '@babel/polyfill';
-import { IStatus } from './interfaces/status';
+// import '@babel/polyfill';
 
 // Base classes
 import { RpcClient } from './rpcClient';
 import { SocketClient } from './socketClient';
 
 // Import models and interfaces
-import { BlockModel } from './models/block';
 import { IBlock } from './interfaces/block';
-import { TxModel } from './models/tx';
 import { ITx } from './interfaces/tx';
+import { BlockModel } from './models/block';
+import { TxModel } from './models/tx';
 
 export interface IGlobalOptions {
   node_rpc: string;
@@ -23,24 +22,23 @@ export interface IGlobalOptions {
  * provides factories of models
  */
 export default class TendermintJs {
-  public status: IStatus; // Last sync node status
+
+  // Static factory methods
+  public static createBlockModel = (rawBlock: IBlock): BlockModel => new BlockModel(rawBlock);
+  public static createTxModel = (rawTx: ITx): TxModel => new TxModel(rawTx);
   public socket: SocketClient;
   public rpc: RpcClient;
 
-  // Static factory methods
-	public static createBlockModel = (rawBlock: IBlock): BlockModel => new BlockModel(rawBlock);
-	public static createTxModel = (rawTx: ITx): TxModel => new TxModel(rawTx);
-
   // Global options
-  private options: IGlobalOptions;
+	private options: IGlobalOptions;
 
-  constructor(options: IGlobalOptions) {
-    this.options = options;
+	constructor(options: IGlobalOptions) {
+		this.options = options;
 
-	  // Create instances
-	  this.socket = new SocketClient(options);
-	  this.rpc = new RpcClient(options);
-  }
+		// Create instances
+		this.socket = new SocketClient(options);
+		this.rpc = new RpcClient(options);
+	}
 
   public test() {
     return 'ok';
