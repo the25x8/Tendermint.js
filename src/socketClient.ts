ff1772e16ba6b7d2f91689c04fcc0e3da5946e2b
cryptoji <kocherginji@gmail.com>
2019-01-27 15:00:05 +0300

    ~ use EventBus in SocketClient connect method

--- src/socketClient.ts
+++ src/socketClient.ts
@@ -1,9 +1,7 @@
-// const { decode } = require('msgpack-lite');
-import { IGlobalOptions } from './index';
-
-const Observable = require('zen-observable');
 const WS = require('ws');
 
+import EventBus from './utils/eventBus';
+import { IGlobalOptions } from './index';
 import { BlockModel } from './models/block';
 import { TxModel } from './models/tx';
 
@@ -17,16 +15,17 @@ export class SocketClient {
 
   // Observer events, only subscription
   // all data should parse and save on your client
-  public $events: any;
+  public $events: EventBus;
   private options: IGlobalOptions; // Global options
   private connection: any; // Ws connection
 
   constructor(options: IGlobalOptions) {
     this.isNode = typeof window === 'undefined';
     this.options = options;
+    this.$events = new EventBus();
   }
 
-	/*
+  /*
 	 * Connect to node via web socket
 	 * subscribe to NewBlocks, NewTxs(optional)
 	 * emit data through observable pattern
@@ -38,141 +37,133 @@ export class SocketClient {
         reject();
       }
 
-	    /*
+      /*
 			 * Create an observable object with
 			 * socket instance and event handlers
+			 * WebSocket events handlers
 			 */
-      this.$events = new Observable((observable) => {
 
-	      /*
-				 * WebSocket events handlers
-				 */
-        // Socket events/messages handler
-        const messageHandler = (event: any) => {
-          // Differences formats of ws class for node and browser
-          const parsedEvent = this.isNode ? JSON.parse(event) : JSON.parse(event.data);
-          const eventData = parsedEvent.result.data;
+	    // Socket events/messages handler
+	    const messageHandler = (event: any) => {
 
-          if (!eventData) {
-            observable.next({
-              type: 'unknown',
-              data: parsedEvent
-            });
-            return;
-          }
+		    // Differences formats of ws class for node and browser
+		    const parsedEvent = this.isNode ? JSON.parse(event) : JSON.parse(event.data);
+		    const eventData = parsedEvent.result.data;
 
-          // Switch models by event types
-          if (eventData.type === 'tendermint/event/NewBlock') {
-            observable.next({
-              type: 'block',
-              data: new BlockModel(eventData.value.block)
-            });
-          } else if (eventData.type === 'tendermint/event/Tx') {
-            observable.next({
-              type: 'tx',
-              data: new TxModel(eventData.value.TxResult)
-            });
-          }
-        };
+		    if (!eventData) {
+			    this.$events.emit('unknown', {
+				    type: 'unknown',
+				    data: parsedEvent,
+			    });
+			    return;
+		    }
 
-        // when socket opened
-        const openHandler = () => {
-          this.isSynced = true;
+		    // Switch models by event types
+		    if (eventData.type === 'tendermint/event/NewBlock') {
+			    this.$events.emit('block', {
+				    type: 'block',
+				    data: new BlockModel(eventData.value.block),
+			    });
+		    } else if (eventData.type === 'tendermint/event/Tx') {
+			    this.$events.emit('tx', {
+				    type: 'tx',
+				    data: new TxModel(eventData.value.TxResult),
+			    });
+		    }
+	    };
 
-          if (this.options.logs) {
-            console.info('Connected to node web socket');
-          }
+	    // when socket opened
+	    const openHandler = () => {
+		    this.isSynced = true;
 
-          // Subscribe to initial events
-          eventsTypes.forEach(
-            (eventType) => this.action('subscribe', eventType)
-          );
+		    if (this.options.logs) {
+			    console.info('Connected to node web socket');
+		    }
 
-          resolve();
-        };
+		    // Subscribe to initial events
+		    eventsTypes.forEach((eventType) => this.action('subscribe', eventType));
 
-        // when socket closed
-        const closeHandler = (error) => {
-          this.connection = null;
-          this.isSynced = false;
+		    resolve();
+	    };
 
-          console.error('TendermintWebSocket/Errors: Error in web socket connection:', error);
-          observable.complete();
-          reject();
-        };
+	    // when socket closed
+	    const closeHandler = (error) => {
+		    this.connection = null;
+		    this.isSynced = false;
 
-        const errorHandler = (error) => {
-          console.error('TendermintWebSocket/Errors: Socket error:', error);
-          observable.complete();
-          reject();
-        };
+		    console.error('TendermintWebSocket/Errors: Error in web socket connection:', error);
+		    reject();
+	    };
 
-	      /*
-				 * Use different realisations to call
-				 * of WebSockets for browser and node
-				 */
-        try {
-          const wsUrl = `${this.options.node_ws}/websocket`;
+	    const errorHandler = (error) => {
+		    console.error('TendermintWebSocket/Errors: Socket error:', error);
+		    reject();
+	    };
 
-          // for node
-          if (this.isNode) {
-            this.connection = new WS(wsUrl);
-            this.connection.on('open', () => openHandler());
-            this.connection.on('close', (error) => closeHandler(error));
-            this.connection.on('error', (error) => errorHandler(error));
-            this.connection.on('message', (event) => messageHandler(event));
-          } else {
-            this.connection = new WebSocket(wsUrl);
-            this.connection.onopen = () => openHandler();
-            this.connection.onclose = (error) => closeHandler(error);
-            this.connection.onerror = (error) => errorHandler(error);
-            this.connection.onmessage = (event) => messageHandler(event);
-          }
+	    /*
+			 * Use different realisations to call
+			 * of WebSockets for browser and node
+			 */
+	    try {
+		    const wsUrl = `${this.options.node_ws}/websocket`;
 
-        } catch (e) {
-          console.error(`TendermintWebSocket/Errors: Error in web socket connection: ${e}`);
-          observable.complete();
-          reject();
-        }
-      });
+		    // for node
+		    if (this.isNode) {
+			    this.connection = new WS(wsUrl);
+			    this.connection.on('open', () => openHandler());
+			    this.connection.on('close', (error) => closeHandler(error));
+			    this.connection.on('error', (error) => errorHandler(error));
+			    this.connection.on('message', (event) => messageHandler(event));
+		    } else {
+			    this.connection = new WebSocket(wsUrl);
+			    this.connection.onopen = () => openHandler();
+			    this.connection.onclose = (error) => closeHandler(error);
+			    this.connection.onerror = (error) => errorHandler(error);
+			    this.connection.onmessage = (event) => messageHandler(event);
+		    }
+
+	    } catch (e) {
+		    console.error(`TendermintWebSocket/Errors: Error in web socket connection: ${e}`);
+		    reject();
+	    }
     });
   }
 
-	/*
+  /*
 	 * Subscribe/Unsubscribe events from Tendermint node
 	 */
   public action(
     method: 'subscribe'|'unsubscribe',
-    type: string
+    type: string,
   ): void {
-	  try {
-		  let methodParams: {
-			  id: string;
-			  query: string;
-		  };
+    try {
+      let methodParams: {
+        id: string;
+        query: string;
+      };
 
-		  if (type === 'blocks') {
-			  methodParams = {
-				  id:  `${method}-out-blocks`,
-				  query: 'tm.event=\'NewBlock\'',
-			  };
-		  } else if (type === 'txs') {
-			  methodParams = {
-				  id:  `${method}-out-txs`,
-				  query: 'tm.event=\'Tx\'',
-			  };
-		  } else {
-			  return;
-		  }
+      if (type === 'blocks') {
+        methodParams = {
+          id:  `${method}-out-blocks`,
+          query: 'tm.event=\'NewBlock\'',
+        };
+      } else if (type === 'txs') {
+        methodParams = {
+          id:  `${method}-out-txs`,
+          query: 'tm.event=\'Tx\'',
+        };
+      } else {
+        return;
+      }
 
-		  this.connection.send(JSON.stringify({
-			  id: methodParams.id,
-			  jsonrpc: '2.0',
-			  params: { query: methodParams.query },
-			  method
-		  }));
-	  } catch (e) {
-		  console.error(e);
-	  }
+      this.connection.send(JSON.stringify({
+        id: methodParams.id,
+        jsonrpc: '2.0',
+        params: { query: methodParams.query },
+        method,
+      }));
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
