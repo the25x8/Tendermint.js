b4da5c040d44faf58a65178c08b6337ae58c3e0a
cryptoji <kocherginji@gmail.com>
2019-02-04 22:15:43 +0300

    + add test to rpcClient.consensusState method

--- test/rpcClient.test.js
+++ test/rpcClient.test.js
@@ -14,11 +14,18 @@ describe('RpcClient endpoints without queriesParams', () => {
       const data = await tm.rpc.abciInfo();
       expect(data).toBeTruthy();
     } catch (e) {
-      fail("Abci Info catch");
+      fail(e);
     }
   });
 
-
+  it('Consensus state', async() => {
+    try {
+      const data = await tm.rpc.consensusState();
+      expect(data).toBeTruthy();
+    } catch (e) {
+      fail(e);
+    }
+  });
 });
 
 describe('RpcClient endpoints with queriesParams', () => {
@@ -28,95 +35,95 @@ describe('RpcClient endpoints with queriesParams', () => {
 /*
  * Test all endpoints
  */
-test('/abci_info', () => {
-
-});
-
-test('/consensus_state', () => {
-
-});
-
-test('/dump_consensus_state', () => {
-
-});
-
-test('/genesis', () => {
-
-});
-
-test('/health', () => {
-
-});
-
-test('/net_info', () => {
-
-});
-
-test('/num_uncofirmed_txs', () => {
-
-});
-
-test('/status', () => {
-
-});
-
-// With query params
-test('/abci_query?height=N&path=X&data=X&prove=X', () => {
-
-});
-
-test('/block?height=N', () => {
-
-});
-
-test('/blockchain?min=N&max=N', () => {
-
-});
-
-test('/broadcast_tx_async?tx=0x0001', () => {
-
-});
-
-test('/broadcast_tx_sync?tx=0x0001', () => {
-
-});
-
-test('/broadcast_tx_commit?tx=0x0001', () => {
-
-});
-
-test('/commit?height=N', () => {
-
-});
-
-test('/consensus_params?height=N', () => {
-
-});
-
-test('/subscribe?query=N', () => {
-
-});
-
-test('/tx?hash=N&prove=N', () => {
-
-});
-
-test('/tx_search?query=X&prove=X&page=X&per_page=X', () => {
-
-});
-
-test('/unconfirmed_txs?limit=N', () => {
-
-});
-
-test('/unsubscribe?query=N', () => {
-
-});
-
-test('/unsubscribe_all', () => {
-
-});
-
-test('/validators?height', () => {
-
-});
+// test('/abci_info', () => {
+//
+// });
+//
+// test('/consensus_state', () => {
+//
+// });
+//
+// test('/dump_consensus_state', () => {
+//
+// });
+//
+// test('/genesis', () => {
+//
+// });
+//
+// test('/health', () => {
+//
+// });
+//
+// test('/net_info', () => {
+//
+// });
+//
+// test('/num_uncofirmed_txs', () => {
+//
+// });
+//
+// test('/status', () => {
+//
+// });
+//
+// // With query params
+// test('/abci_query?height=N&path=X&data=X&prove=X', () => {
+//
+// });
+//
+// test('/block?height=N', () => {
+//
+// });
+//
+// test('/blockchain?min=N&max=N', () => {
+//
+// });
+//
+// test('/broadcast_tx_async?tx=0x0001', () => {
+//
+// });
+//
+// test('/broadcast_tx_sync?tx=0x0001', () => {
+//
+// });
+//
+// test('/broadcast_tx_commit?tx=0x0001', () => {
+//
+// });
+//
+// test('/commit?height=N', () => {
+//
+// });
+//
+// test('/consensus_params?height=N', () => {
+//
+// });
+//
+// test('/subscribe?query=N', () => {
+//
+// });
+//
+// test('/tx?hash=N&prove=N', () => {
+//
+// });
+//
+// test('/tx_search?query=X&prove=X&page=X&per_page=X', () => {
+//
+// });
+//
+// test('/unconfirmed_txs?limit=N', () => {
+//
+// });
+//
+// test('/unsubscribe?query=N', () => {
+//
+// });
+//
+// test('/unsubscribe_all', () => {
+//
+// });
+//
+// test('/validators?height', () => {
+//
+// });
